66e237425ad74f4e52052febd0c982dd09172cec
Kundogy <jared.a.kun@gmail.com>
2022-12-05 18:39:50 -0800

    Adding tests, still working on.

--- src/main/java/Tests/Tests.java
+++ src/main/java/Tests/Tests.java
@@ -14,8 +14,37 @@ public class Tests {
         if (machines.vendingMachines.size() == 0)
             fail("No vending machines loaded");
 
+        //checks if prompt works
         if (purchase.contains("Please insert: $"))
             success("Purchase with 0 dollars worked");
+        //checks if machine is online
+        if(machine.online == true)
+            success("Vending machine online");
+        //checks the vending machine location based on input
+        if(machine.location.address.contains("Shaffer"))
+            success("Location is 8018 Shaffer Locks Apt. 172");
+//        if(machine.location.address.contains("Smith")) //not working as intended
+//            success("Wrong tester");
+        //checks if vending machine is empty
+        if (machines.vendingMachines.isEmpty()== true)
+            fail("Vending Machine is empty");
+        //checks the price of an item in a particular row and column
+        if(machine.inventory.getPrice(0,0) == 1.00)
+            success("Price of Item in 1A is $1");
+
+
+        //working on still, second portion of change dispensing is issue.
+        System.out.println(machine.purchaseItem(0,1,2.00));
+        if(machine.purchaseItem(0,1,1.5) == "Check below for item")
+            success("Item Purchased");
+
+        if(machine.purchaseItem(0,1,2.00) == "Dispensing item below and change of: $0.5")
+            success("Item Purchased, change dispensed");
+
+        // to be uncommented later
+//        System.out.println(machine.inventory.toString());
+//        System.out.println(machine.toString());
+//        System.out.println(machine.inventory.getExpiredItems());
 
         if (failed)
             System.exit(1);
