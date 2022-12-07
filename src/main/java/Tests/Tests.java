61a82e00ad7b9af5e178e50f06a627b1cbf5c34b
Kundogy <jared.a.kun@gmail.com>
2022-12-06 21:18:03 -0800

    Added more tests, fixed for new data set. Finished GUI testing as well.

--- src/main/java/Tests/Tests.java
+++ src/main/java/Tests/Tests.java
@@ -2,6 +2,7 @@ package Tests;
 
 import VendingMachine.VendingMachine;
 import VendingMachine.VendingMachines;
+import java.util.Objects;
 
 public class Tests { //certain tests need their inputs changed if you change from vending machine ID : 1
     public static boolean failed = false;
@@ -23,32 +24,47 @@ public class Tests { //certain tests need their inputs changed if you change fro
             success("Vending machine online");
 
         //checks the vending machine location based on input
-        if(machine.location.address.contains("Spencer"))
-            success("Location is 5140 Spencer Islands");
+        if(machine.location.address.contains("Alexander"))
+            success("Location is 527 Alexander Highway");
 
-//        if(machine.location.address.contains("Smith")) //not working as intended
-//            success("Wrong tester");
+        // Purposely gives an incorrect address : is meant to fail
+        if(machine.location.address.contains("Smith") == false)
+            fail("Not correct address");
+
         //checks if vending machine is empty
         if (machines.vendingMachines.isEmpty())
             fail("Vending Machine is empty");
 
+        if (machines.vendingMachines.isEmpty() == false)
+            success("Vending Machine is not empty");
+
         //checks the price of an item in a particular row and column
-        if(machine.inventory.getPrice(0,0) == 5.0)
-            success("Price of Item in 1A is $5.0");
+        if(machine.inventory.getPrice(0,0) == 1.00)
+            success("Price of Item in 1A is $1.00");
 
+        //Purchases an item and checks if the correct message gets displayed (if you change the data set this breaks.)
+        if(Objects.equals(machine.purchaseItem(0, 0, 1.00), "Dispensing item below and change of: $0.0"))
+            success("Dispensing item below");
 
-        //working on still, second portion of change dispensing is issue.
-        if(machine.purchaseItem(0,1,1.5) == "Check below for item")
-            success("Item Purchased");
+        if(Objects.equals(machine.purchaseItem(0, 1, 2.00), "Dispensing item below and change of: $1.0"))
+            success("Dispensing item below and change of: $1.0");
 
+        // checks quantity of items in a particular row and column
+        if(machine.inventory.getQuantity(0,0) == 5)
+            success("Quantity of Items in 1A is 5");
+
+        //checks if the queue has items
+        if(machine.queuedItems.queuedItems.isEmpty() == true)
+            success("No Items in Queue");
+
+        //Just test print statements
+        System.out.println(machine.inventory.toString());
+        System.out.println(machine.toString());
+        System.out.println(machine.inventory.getExpiredItems());
+        System.out.println(machine.inventory.getItem(0,0,1));
         System.out.println(machine.purchaseItem(0,1,2.00));
-        if(machine.purchaseItem(0,1,2.00) == "Dispensing item below and change of: $0.5")
-            success("Item Purchased, change dispensed");
-
-        // to be uncommented later
-//        System.out.println(machine.inventory.toString());
-//        System.out.println(machine.toString());
-//        System.out.println(machine.inventory.getExpiredItems());
+        System.out.println(machine.inventory.getPrice(0,0));
+        System.out.println(machine.inventory.getQuantity(0,0));
 
         if (failed)
             System.exit(1);
